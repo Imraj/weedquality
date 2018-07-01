fee52ec19cc6e84af1a33322cac766314597ea8d
imraj3 <imraj3@live.co.uk>
2018-07-01 03:17:39 +0100

    l

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { CameraPage } from "../pages/camera/camera";
 
 import { Camera } from "@ionic-native/camera";
 import { CameraPreview } from "@ionic-native/camera-preview";
-
+import { HTTP } from '@ionic-native/http';
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +22,8 @@ import { CameraPreview } from "@ionic-native/camera-preview";
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -34,6 +36,7 @@ import { CameraPreview } from "@ionic-native/camera-preview";
     StatusBar,
     SplashScreen,
     Camera,
+    HTTP,
     CameraPreview,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 import { HTTP } from '@ionic-native/http';
+import { Http,Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
 /**
  * Generated class for the ResultPage page.
  *
@@ -16,29 +19,49 @@ import { HTTP } from '@ionic-native/http';
 })
 export class ResultPage {
 
+  concepts : any
   imageData : any
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HTTP,
-              public alertCtrl: AlertController) {
+
+  mariPresent: any
+  mariValue: any
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public alertCtrl: AlertController, private http: Http) {
+
+    var app = this
     this.imageData = this.navParams.get("image")
-
-    this.http.get("https://ratemyweed.herokuapp.com/process",{},{})
-        .then(data=>{
-           let alert = this.alertCtrl.push({
-             title:"Res",
-             message:data,
-             buttons: ['OK']
-           })
-           alert.present()
-        })
-        .catch(error=>{
-
-        });
 
   }
 
   ionViewDidLoad() {
+    var app = this
     console.log('ionViewDidLoad ResultPage');
     this.imageData = this.navParams.get("image")
+
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    var data = 'imageData=' + this.imageData;
+    this.http.post("https://ratemyweed.herokuapp.com/process",data,{headers:headers})
+        .subscribe((res)=>{
+
+           console.log("res-json")
+
+           var result = JSON.parse((<any>res)._body)
+           var concepts = result.outputs[0].data.concepts
+           app.concepts = concepts
+           console.log(concepts)
+           for(var i=0;i<concepts.length;i++){
+             let cname = concepts[i].name
+             let cvalue = concepts[i].value
+             console.log(cname,cvalue)
+             if(cname == "marijuana"){
+               app.mariPresent = true
+               app.mariValue = cvalue
+             }
+           }
+
+
+        })
   }
 
 }
